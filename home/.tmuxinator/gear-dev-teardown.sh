26ac75a5cac4e6cf3fed645a4c60d119de75c13f
keith.beckman <keith.beckman@ge.com>
2017-04-20 13:32:51 -0500

    TMUXINATOR - removing gear-dev project + scripts

--- home/.tmuxinator/gear-dev-teardown.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker_machines=( default etcd swarm-master1 swarm-master2 )
-#docker_machines=$(docker-machine ls --quiet)
-
-for machine in ${docker_machines[@]}; do
-  docker-machine stop ${machine}
-done
-
