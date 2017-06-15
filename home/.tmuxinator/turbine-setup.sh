15c9e05b1de2a13eb858f67157a10a0f7b88390f
keith.beckman <keith.beckman@ge.com>
2017-06-15 12:38:03 -0500

    TMUXINATOR - turbine project changes for 'turbine-web'

--- home/.tmuxinator/turbine-setup.sh
+++ home/.tmuxinator/turbine-setup.sh
@@ -10,5 +10,6 @@ eval $(docker-machine env default)
 
 docker-machine-fix-ip --machine default --ipaddress 192.168.99.105
 
-docker-compose -f ~/Source-Code/github.build.ge.com/cloud-center/turbine-ui/development-compose.yml up -d
+docker-compose -f ~/Source-Code/github.build.ge.com/cloud-center/turbine-web/development-compose.yml up -d
+docker-compose -f ~/Source-Code/github.build.ge.com/cloud-center/gear/development-compose.yml up -d
 
